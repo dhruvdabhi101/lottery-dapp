85ded006595a8b11140031343c08de91a8500421
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-07-06 01:11:23 +0530

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Heading, Image } from "@chakra-ui/react";
 import { useWeb3ModalAccount } from "@web3modal/ethers/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -11,8 +12,17 @@ export default function Home() {
     if (isConnected) push('/home')
   },[isConnected])
   return (
-    <div>
-      <w3m-button />
+    <div className="flex flex-col w-full justify-center p-3">
+      <div className="flex self-end p-3">
+       <w3m-button />
+  `   </div>
+      <div className="w-full h-full flex flex-col gap-7">
+        <Heading as="h1" fontSize={['3xl','5xl','7xl']} textAlign="center" mb={4} >
+          Stake your Deez 
+          </Heading>
+        <Image src={'/frontpage.png'} className="border-[3px] border-black shadow-lg w-[90%] h-[90%]  md:w-[60%] md:h-[60%] self-center" />
+      </div>
+    <Image src="/arrow.svg" className="absolute size-10 md:w-20 md:h-20  right-40 top-10 -rotate-45" />
     </div>
   );
 }
